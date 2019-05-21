b1a8b4ac653f93a2ca836ff6beb2d93676788acb
alex <amanukya@student.42.fr>
2019-05-21 12:31:28 -0700

    allTeams Query

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ import Sequelize from 'sequelize';
 
 const sequelize = new Sequelize('slack', 'alex', '', {
   dialect: 'postgres',
+  operatorsAliases: Sequelize.Op,
   define: {
     underscored: true,
   },
