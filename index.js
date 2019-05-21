7b1fe278881976cdec45c8093435535d17c00263
alex <amanukya@student.42.fr>
2019-05-20 17:38:40 -0700

    Login Mutation GraphQL

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import cors from 'cors';
 
 import models from './models';
 
+const SECRET = 'asiodfhoi1hoi23jnl1kejd';
+const SECRET2 = 'asiodfhoi1hoi23jnl1kejasdjlkfasdd';
+
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
 
 const resolvers = mergeResolvers(
@@ -20,6 +23,8 @@ const server = new ApolloServer({
     user: {
       id: 1,
     },
+    SECRET,
+    SECRET2,
   },
 });
 
